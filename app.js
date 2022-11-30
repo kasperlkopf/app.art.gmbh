1a984cf06518b7fadd077607ad21449a7d5a34a2
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-11-30 22:55:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // app.js
 
-import router from '/modules/router.js';
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('/components/Index.js'),
+    },
+    {
+      path: '/lieferprobleme',
+      component: () => import('/components/DeliveryProblems.js'),
+    },
+    {
+      path: '/warehouse',
+      component: () => import('/components/Warehouse.js'),
+    },
+  ],
+});
+
+router.beforeEach((to, from) => {
+  // document.title = to.meta.title;
+});
 
 const app = Vue.createApp({
   name: 'App',
