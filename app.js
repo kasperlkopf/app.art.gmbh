7883e3125f65f3fdafa7c11d2b9f311b708b9a42
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-11-30 22:49:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
 // app.js
 
 import router from '/modules/router.js';
-import store from '/modules/store.js';
-
-import SharedMethods from '/components/SharedMethods.js';
-import Modal from '/components/Modal.js';
 
 const app = Vue.createApp({
   name: 'App',
-  mixins: [SharedMethods],
-  components: {
-    Modal,
-  },
   data() {
     return {
-      currentDate: this.formatDate(new Date()),
-      connectionStatus: '',
+      //
     };
   },
   created() {
     console.log('App: created');
   },
-  mounted() {
-    console.log('App: mounted');
-  },
-  methods: {
-    //
-  },
 });
 
 app.use(router);
-app.use(store);
-
 app.mount('#app');
