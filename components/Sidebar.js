a8a7ba2b354f20f7037166b49095a29dd7c8acfc
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-12-29 18:06:00 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,9 @@
 // Sidebar.js
 
 const template = `
-  <div id="sidebar" class="col-3">
-    <div class="list-group">
-      <router-link to="warehouse" class="list-group-item list-group-item-action">Warehouse</router-link>
-      <router-link to="turnover" class="list-group-item list-group-item-action">Turnover</router-link>
-    </div>
+  <div class="list-group">
+    <router-link to="warehouse" class="list-group-item list-group-item-action">Warehouse</router-link>
+    <router-link to="turnover" class="list-group-item list-group-item-action">Turnover</router-link>
   </div>
 `;
 
