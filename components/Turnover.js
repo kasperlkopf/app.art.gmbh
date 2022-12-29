37fbe2a0b7c9f1722b38a68f30f460287d333ee4
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-12-29 18:47:05 +0100

    Update Turnover.js

--- components/Turnover.js
+++ components/Turnover.js
@@ -3,7 +3,7 @@
 import SharedMethods from '/components/SharedMethods.js';
 
 const template = `
-  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+  <div class="row row-cols-1 row-cols-md-2 g-4">
 
     <div class="col">
       <div class="card">
