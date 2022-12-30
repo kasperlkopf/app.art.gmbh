5924d8e16bac79122aafceb2a1ee6fd1b82624bc
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-12-30 13:12:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      component: () => import('/components/Index.js'),
-    },
-    {
       path: '/lieferprobleme',
       component: () => import('/components/DeliveryProblems.js'),
     },
