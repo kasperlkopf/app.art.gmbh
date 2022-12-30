c9b24a4e2385ec6efac6291538c0c94aa9860a7b
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-12-30 18:28:13 +0100

    Updates

--- app.js
+++ app.js
@@ -38,45 +38,6 @@ const app = createApp({
   },
   created() {
     console.log('App: created');
-
-    /*
-    const auth = localStorage.getItem('auth');
-
-    if (!auth) {
-      const pw = prompt('Passwort');
-
-      if (btoa(pw) === 'a29tYmlzY2hyYXViZW4=') {
-        localStorage.setItem('isAuthenticated', true)
-      } else {
-        document.body.innerHTML = '';
-      }
-    }
-    */
-
-    this.setTheme();
-  },
-  methods: {
-    setTheme(t) {
-      const theme = t ? t : this.getPreferredTheme();
-
-      localStorage.setItem('theme', theme);
-      document.documentElement.setAttribute('data-bs-theme', theme);
-    },
-    getPreferredTheme() {
-      const storedTheme = localStorage.getItem('theme');
-
-      if (storedTheme) {
-        return storedTheme;
-      }
-
-      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-    },
-    toggleTheme() {
-      const currentTheme = document.documentElement.getAttribute('data-bs-theme');
-      const theme = currentTheme ? currentTheme === 'dark' ? 'light' : 'dark' : this.getPreferredTheme();
-
-      this.setTheme(theme);
-    }
   },
 });
 
