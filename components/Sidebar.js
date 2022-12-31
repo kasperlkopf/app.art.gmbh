74ce66740d344233b225049795a2ad457994ffd1
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2022-12-31 09:28:38 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,7 +4,7 @@ const template = `
   <aside class="d-flex flex-column text-nowrap user-select-none overflow-auto position-sticky" style="top: 81px;">
     <nav class="nav flex-column nav-pills mb-auto">
       <router-link to="warehouse" class="nav-link adios-nav-link mb-1" active-class="active">Lager</router-link>
-      <router-link to="turnover" class="nav-link adios-nav-link mb-1" active-class="active">Umsätze</router-link>
+      <router-link to="turnover" class="nav-link adios-nav-link mb-1" active-class="active">Umsatz</router-link>
     </nav>
   </aside>
 `;
