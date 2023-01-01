8641f83e8d2d1682fa510b1ee8da3f36d9b7931e
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-01 16:06:31 +0100

    Updates

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,10 @@
 // Sidebar.js
 
 const template = `
-  <aside class="d-flex flex-column text-nowrap user-select-none overflow-auto position-sticky" style="top: 81px;">
+  <aside class="d-flex flex-column text-nowrap user-select-none overflow-auto">
     <div id="sidebar" class="offcanvas-md offcanvas-bottom">
       <div class="offcanvas-body">
-        <nav class="nav flex-column nav-pills mb-auto">
+        <nav class="nav flex-column nav-pills mb-auto w-100">
           <router-link to="warehouse" class="nav-link mb-1" active-class="active">Lager</router-link>
           <router-link to="turnover" class="nav-link mb-1" active-class="active">Umsatz</router-link>
         </nav>
