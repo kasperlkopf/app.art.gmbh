25ad07ac1d42e1d9f8c6ea08e65c4e61f2905a1f
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-02 12:50:09 +0100

    Updates

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ const template = `
   <header class="navbar navbar-expand-md sticky-top border-bottom bg-body top-0 bg-opacity-75" style="backdrop-filter: blur(12px);">
     <nav class="container flex-wrap flex-lg-nowrap">
 
-      <button class="btn btn-link nav-link p-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar">
+      <button class="navbar-toggler nav-link p-2 border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar">
         <i class="bi bi-list"></i>
       </button>
 
