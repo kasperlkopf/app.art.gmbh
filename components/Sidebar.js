4c3b7dd855703ebfb15f2de92f20ce157c99ea11
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-02 13:21:47 +0100

    Uüdates

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,7 +6,7 @@ const template = `
       <div class="offcanvas-header">
         <h5 class="offcanvas-title">Navigation</h5>
       </div>
-      <div class="offcanvas-body container">
+      <div class="offcanvas-body">
         <nav class="nav flex-column nav-pills mb-auto w-100">
           <router-link to="warehouse" class="nav-link mb-1" active-class="active">Lager</router-link>
           <router-link to="turnover" class="nav-link mb-1" active-class="active">Umsatz</router-link>
