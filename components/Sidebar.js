bee2adceb1a05318fccb23bc353c906a40c97c1f
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-03 22:28:29 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,13 +7,13 @@ const template = `
     </div>
     <div class="offcanvas-body">
       <nav class="nav flex-column nav-pills mb-auto w-100">
-        <router-link v-if="isOkay" to="warehouse" class="nav-link mb-1" active-class="active">Lager</router-link>
-        <router-link v-if="isOkay" to="turnover" class="nav-link mb-1" active-class="active">Umsatz</router-link>
-        <h6 class="d-flex align-items-center fw-semibold mt-4 mb-3">
+        <router-link v-if="isAllowed || currentPath === 'warehouse'"" to="warehouse" class="nav-link mb-1" active-class="active">Lager</router-link>
+        <router-link v-if="isAllowed || currentPath === 'turnover'" to="turnover" class="nav-link mb-1" active-class="active">Umsatz</router-link>
+        <h6 class="d-none align-items-center fw-semibold mt-4 mb-3">
           <i class="bi bi-code-slash me-2"></i> 
           Experimental
         </h6>
-        <router-link v-if="isOkay" to="test" class="nav-link mb-1" active-class="active">Test</router-link>
+        <router-link v-if="isAllowed || currentPath === 'test'" to="test" class="nav-link mb-1" active-class="active">Test</router-link>
       </nav>
     </div>
   </aside>
@@ -27,7 +27,10 @@ export default {
     };
   },
   computed: {
-    isOkay() {
+    currentPath() {
+      return this.$route.path.slice(1);
+    },
+    isAllowed() {
       return !this.$route.meta.isRestricted;
     },
   },
