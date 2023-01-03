d12e70d350c575c945e2277063b42e0b6de9e227
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-03 20:57:56 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,7 @@
 // Sidebar.js
 
 const template = `
+{{ isOkay }}
   <aside id="sidebar" class="offcanvas-md offcanvas-start text-nowrap user-select-none overflow-auto position-fixed py-md-3 pe-md-3" style="width: 256px;">
     <div class="offcanvas-header">
       <h5 class="offcanvas-title">Navigation</h5>
@@ -25,6 +26,13 @@ export default {
     return {
       //
     };
+  },
+  computed: {
+    isOkay() {
+      const notAllowed = ['warehouse'];
+
+      return this.$route;
+    },
   },
   created() {
     console.log('Sidebar: created');
