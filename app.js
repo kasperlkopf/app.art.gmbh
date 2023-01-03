95bfb5ad9c430c693abf555d7c45c49fd6afa7a8
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-03 15:16:20 +0100

    Updates

--- app.js
+++ app.js
@@ -1,53 +1,11 @@
 // app.js
 
+import router from '/router.js';
+
 import Header from '/components/Header.js';
 import Sidebar from '/components/Sidebar.js';
 
-const { createRouter, createWebHistory } = VueRouter;
-const { createApp } = Vue;
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      component: () => import('/components/Index.js'),
-    },
-    {
-      path: '/status',
-      component: () => import('/components/Status.js'),
-    },
-    {
-      path: '/lieferprobleme',
-      component: () => import('/components/DeliveryProblems.js'),
-    },
-    {
-      path: '/warehouse',
-      component: () => import('/components/Warehouse.js'),
-    },
-    {
-      path: '/turnover',
-      component: () => import('/components/Turnover.js'),
-    },
-    {
-      path: '/test',
-      component: () => import('/components/Test.js'),
-    },
-  ],
-});
-
-router.beforeEach((to, from) => {
-  //
-});
-
-router.afterEach((to, from) => {
-  const sidebar = document.querySelector('#sidebar');
-  const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(sidebar);
-
-  offcanvas.hide();
-});
-
-const app = createApp({
+const app = Vue.createApp({
   name: 'App',
   components: {
     Header,
---
+++ router.js
@@ -0,0 +1,44 @@
+// router.js
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('/components/Index.js'),
+    },
+    {
+      path: '/status',
+      component: () => import('/components/Status.js'),
+    },
+    {
+      path: '/lieferprobleme',
+      component: () => import('/components/DeliveryProblems.js'),
+    },
+    {
+      path: '/warehouse',
+      component: () => import('/components/Warehouse.js'),
+    },
+    {
+      path: '/turnover',
+      component: () => import('/components/Turnover.js'),
+    },
+    {
+      path: '/test',
+      component: () => import('/components/Test.js'),
+    },
+  ],
+});
+
+router.beforeEach((to, from) => {
+  //
+});
+
+router.afterEach((to, from) => {
+  const sidebar = document.querySelector('#sidebar');
+  const offcanvas = bootstrap.Offcanvas.getOrCreateInstance(sidebar);
+
+  offcanvas.hide();
+});
+
+export default router;
