32e35cfc65d2532d5dc31af0eed5fa2b6ae4b303
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-04 11:28:05 +0100

    Update Sidebar.js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -8,11 +8,6 @@ const template = `
     <div class="offcanvas-body">
       <nav class="nav flex-column nav-pills mb-auto w-100">
         <router-link v-for="route in availableRoutes" :to="route.path" class="nav-link mb-1" active-class="active">{{ route.name }}</router-link>
-        <hr>
-        <router-link v-if="!requiresAuth || currentPath === 'status'" to="status" class="nav-link mb-1" active-class="active">Status</router-link>
-        <router-link v-if="!requiresAuth || currentPath === 'turnover'" to="turnover" class="nav-link mb-1" active-class="active">Umsatz</router-link>
-        <router-link v-if="!requiresAuth || currentPath === 'warehouse'" to="warehouse" class="nav-link mb-1" active-class="active">Lager</router-link>
-        <router-link v-if="!requiresAuth || currentPath === 'lorem-ipsum'" to="lorem-ipsum" class="nav-link mb-1" active-class="active">Lorem ipsum</router-link>
       </nav>
     </div>
   </aside>
