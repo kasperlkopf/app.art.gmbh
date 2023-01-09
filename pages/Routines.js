f502cfcd8858df3221cac85f60083e2afbf7840a
kasperlkopf <98323986+kasperlkopf@users.noreply.github.com>
2023-01-09 14:38:26 +0100

    Updates

--- pages/Routines.js
+++ pages/Routines.js
@@ -5,23 +5,17 @@ import StatusIcon from '../components/StatusIcon.js';
 const template = `
   <div class="routines">
 
-    <div v-for="routine in routines" class="mb-4">
-      <header class="d-flex align-items-center mb-2">
-        <div v-html="routine.icon" class="d-flex me-2"></div>
+    <div v-for="routine in routines" class="list-group mb-4">
+      <header class="d-flex align-items-center list-group-item">
+        <i class="bi me-2" :class="routine.icon"></i>
         <span class="fw-semibold">{{ routine.title }}</span>
       </header>
+      <button v-for="action in routine.actions" type="button" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" @click="handleClick(action)" :disabled="isBusy">
+        <span class="text-truncate">{{ action.name }}</span>
 
-      <div class="list-group">
-        <button v-for="action in routine.actions" type="button" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" @click="handleClick(action)" :disabled="isBusy">
-          <span class="text-truncate">{{ action.name }}</span>
-
-          <StatusIcon v-if="['loading', 'success', 'error'].includes(action.status)" :status="action.status" />
-
-          <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
-          </svg>
-        </button>
-      </div>
+        <StatusIcon v-if="['loading', 'success', 'error'].includes(action.status)" :status="action.status" />
+        <i v-else class="bi bi-arrow-right-short"></i>
+      </button>
     </div>
 
   </div>
@@ -38,11 +32,7 @@ export default {
       routines: [
         {
           title: 'Outlook',
-          icon: `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-              <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-            </svg>
-          `,
+          icon: 'bi-envelope',
           actions: [
             {
               name: 'Offene Bestellungen bündeln',
@@ -68,11 +58,7 @@ export default {
         },
         {
           title: 'myfactory',
-          icon: `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle-fill" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
-            </svg>
-          `,
+          icon: 'bi-play-circle-fill',
           actions: [
             {
               name: 'Geisterpreise löschen',
@@ -106,11 +92,7 @@ export default {
         },
         {
           title: 'PEET',
-          icon: `
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
-              <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" />
-            </svg>
-          `,
+          icon: 'bi-box-seam',
           actions: [
             {
               name: 'Gelöschte KS loggen',
